4477d8cd1a3bd71de485ace7e62bdc60fc7f4995
kpv <kpv@50aeb178-8500-dd11-b8ad-001a4d4a7278>
2011-12-02 06:33:16 +0000

    Володя почистил скрипт и добавил паузы

--- scan-dir.pl
+++ scan-dir.pl
@@ -7,35 +7,32 @@ $scan_dir = $ARGV[0] if defined $ARGV[0];
  
 die "Error. Dir $scan_dir not found." unless (-e $scan_dir);
  
-scan_dir ($scan_dir,"d");
+&scan_dir($scan_dir);
  
 exit 0;
  
 sub scan_dir
 {
+    sleep .2; 
  
-    my ($_dir, $_mode) = @_;
-    my @list;
- 
-    my $mode = "a";
     my $dir = ".";
-    my $counter = 0;
+    my $counter = 0; 
+    my $ceil = 0;
  
-    $mode = $_mode if defined $_mode;
-    $dir = $_dir if defined $_dir;
+    $dir = $_[0] if defined $_[0];
     #print "$dir\n";
     for my $file (glob($dir.'/*'))
     {
- 
         next if -l $file;
         if (-d $file)
         {
-            scan_dir($file,$mode);
+            &scan_dir($file);
         }
-        next unless -f $file;
         $counter++;
+	$ceil = $counter/10000;
+	sleep .2 if (int $ceil == $ceil);
     }
-   if ($counter>8000){
-    print "$counter $dir\n";
-   }
+    if ($counter>8000){
+        print "$counter $dir\n";
+    }
 }
